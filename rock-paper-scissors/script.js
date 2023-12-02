afcedfcfbb1c8131b1f2a2eba2bbd3a6fd3428b7
Chad Smith <chad.smith@ymail.com>
2023-12-02 21:40:09 +0800

    Finish code for user selections paper and scissors Enhance the CSS layout further with more visual elements

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -24,29 +24,59 @@ userPick.addEventListener('click', function () {
     userPick = "rock";
     gameStart(userPick, computerChoice());
 });
+userPick = document.querySelector('.choice.paper');
+userPick.addEventListener('click', function () {
+    userPick = "paper";
+    gameStart(userPick, computerChoice());
+});
+userPick = document.querySelector('.choice.scissors');
+userPick.addEventListener('click', function () {
+    userPick = "scissors";
+    gameStart(userPick, computerChoice());
+});
 
 // Setting up a text field to get game status updates instead of printing to console
 let gameText = document.querySelector('.game-text');
+let playerSelectionText = document.querySelector('.player-pick');
+let computerSelectionText = document.querySelector('.computer-pick');
+let roundText = document.querySelector('.round-text');
+let playerPointText = document.querySelector('.player-points');
+let computerPointText = document.querySelector('.computer-points');
 
 
 //Function for the game logic
 function gameStart(userPick, computerPick) {
+    roundText.innerText = `Round
+${round}`;
 
     if (userPick === computerPick) {
+        playerSelectionText.innerText = `" ${userPick} "`;
+        computerSelectionText.innerText = `" ${computerPick} "`;
         gameText.innerText = `You picked ${userPick}
         Computer chose ${computerPick}
         It's a Tie! Restart Round!`;
         round -= 1;
+
     } else if ((userPick === "rock" && computerPick === "scissors") ||
         (userPick === "paper" && computerPick === "rock") ||
         (userPick === "scissors" && computerPick === "paper")) {
         playerPoints += 1;
+
+        playerPointText.innerText = `Player
+        ${playerPoints}`;
+        playerSelectionText.innerText = `" ${userPick} "`;
+        computerSelectionText.innerText = `" ${computerPick} "`;
         gameText.innerText = `You picked ${userPick}
         Computer chose ${computerPick}
         You win round ${round}!`;
 
     } else {
         computerPoints += 1;
+        computerPointText.innerText = `Computer
+        ${computerPoints}`;
+
+        playerSelectionText.innerText = `" ${userPick} "`;
+        computerSelectionText.innerText = `" ${computerPick} "`;
         gameText.innerText = `You picked ${userPick}
         Computer chose ${computerPick}
         You lose round ${round}!`;
