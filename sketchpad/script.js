69cf50758d5594a2eaa58a78192e3a1f44966d56
Chad Smith <chad.smith@ymail.com>
2023-12-13 15:52:06 +0800

    Add button to enable a fun rainbow mode rainbow mode creates random colors on sketchpad and changes background color

--- sketchpad/script.js
+++ sketchpad/script.js
@@ -1,6 +1,12 @@
 const container = document.querySelector('.container');
+const button = document.querySelectorAll('.rainbow-button');
+const body = document.body;
+const audio = new Audio('rainbow.mp3');
+
 let isLeftMouseDown = false;
 let isRightMouseDown = false;
+
+let rainbowMode = false;
 
 // function that will take in user defined dimensions and systematically create a row n times and appenend n cells to each row to make a perfect grid
 function gridBuild(dimension) {
@@ -29,21 +35,67 @@ function userInput() {
     gridBuild(userDimension)
 }
 
+// functions to apply changes to both buttons
+function changeButton() {
+
+    if (rainbowMode === false) {
+        body.style.background = 'linear-gradient(to right, red, orange, yellow, green, blue, indigo)';
+        body.style.backgroundSize = '200% 200%';
+        body.style.animation = 'gradientShift 15s linear infinite';
+
+        playAudio()
+
+        button.forEach(button => {
+            button.textContent = "RAINBOW MODE: ACTIVATED!";
+        });
+
+        rainbowMode = true;
+    } else {
+        body.style.background = 'white';
+        body.style.backgroundSize = '100%';
+        body.style.animation = 'none';
+
+        button.forEach(button => {
+            button.textContent = 'DO NOT PRESS!';
+        });
+
+        rainbowMode = false;
+    }
+}
+
+//Function to play audio on button click
+function playAudio() {
+    audio.play()
+}
+
 // Create a series of mouse events, mousedown, mouseover and mouseup to begin drawing and end drawing and right clicking to return the cell back to white.
 container.addEventListener('mousedown', event => {
     if (event.button === 0 && event.target.classList.contains('grid-cell')) {
-        event.target.style.backgroundColor = 'black';
-        isLeftMouseDown = true;
+        if (rainbowMode === false) {
+            event.target.style.backgroundColor = 'black';
+            isLeftMouseDown = true;
+        } else {
+            let randomColorR = Math.floor(Math.random() * 256);
+            let randomColorG = Math.floor(Math.random() * 256);
+            let randomColorB = Math.floor(Math.random() * 256);
+            event.target.style.backgroundColor = `rgb(${randomColorR}, ${randomColorG}, ${randomColorB})`;
+            isLeftMouseDown = true;
+        }
     }
     event.preventDefault();
 });
-
 container.addEventListener('mouseover', event => {
     if (isLeftMouseDown && event.target.classList.contains('grid-cell')) {
-        event.target.style.backgroundColor = 'black';
+        if (rainbowMode === false) {
+            event.target.style.backgroundColor = 'black';
+        } else {
+            let randomColorR = Math.floor(Math.random() * 256);
+            let randomColorG = Math.floor(Math.random() * 256);
+            let randomColorB = Math.floor(Math.random() * 256);
+            event.target.style.backgroundColor = `rgb(${randomColorR}, ${randomColorG}, ${randomColorB})`;
+        }
     }
 });
-
 document.addEventListener('mouseup', event => {
     if (event.button === 0) {
         isLeftMouseDown = false;
@@ -58,23 +110,28 @@ container.addEventListener('mousedown', event => {
     }
     event.preventDefault();
 });
-
 container.addEventListener('mouseover', event => {
     if (isRightMouseDown && event.target.classList.contains('grid-cell')) {
         event.target.style.backgroundColor = 'white';
     }
 });
-
 document, addEventListener('mouseup', event => {
     if (event.button === 2) {
         isRightMouseDown = false;
     }
 });
-
 container.addEventListener('contextmenu', event => {
     event.preventDefault();
 })
+button.forEach(button => {
+    button.addEventListener('click', changeButton);
+});
+
 
 
 userInput()
 
+
+/*
+instead of pop ups display instructions and a button to start on page with dimension selector
+*/ 
